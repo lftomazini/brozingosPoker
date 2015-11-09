3735532131a84c87e6368a4156c4ad152fff43cd
Leonardo Viglino <lbv001@brki164-lnx-26.bucknell.edu>
2015-11-09 10:17:07 -0500

    initial commit

---
+++ src/csci205finalproject/GameClass.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csci205finalproject;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author lbv001
+ */
+public class GameClass {
+    private ArrayList<Double> playerList;
+    private int numOfRounds;
+
+    public void nextRound() {
+        numOfRounds++;
+    }
+
+    public int getNumOfRounds() {
+        return numOfRounds;
+    }
+
+    public ArrayList<Double> getPlayerList() {
+        return playerList;
+    }
+
+    public void setPlayerList(ArrayList<Double> playerList) {
+        this.playerList = playerList;
+    }
+
+}
