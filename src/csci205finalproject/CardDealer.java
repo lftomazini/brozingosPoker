70be7289db313234a6ed3ccf27aa71f98e396377
Leonardo Viglino <lbv001@brki164-lnx-21.bucknell.edu>
2015-11-11 10:50:19 -0500

    new CardDealer

--- src/csci205finalproject/CardDealer.java
+++ src/csci205finalproject/CardDealer.java
@@ -5,6 +5,7 @@
  */
 package csci205finalproject;
 
+import Cards.Card;
 import java.util.ArrayList;
 import java.util.Random;
 import package Cards;
@@ -13,76 +14,57 @@ import package Cards;
  * @author as062
  */
 public class CardDealer {
-    
-    private Cards[] deck;
+
+    private Card[] deck;
     private Player[] players;
     private int rounds;
-    private ArrayList<Integer> cards_chosen = new ArrayList<>(); 
-    private ArrayList<Cards> cardsOnTable = new ArrayList<>();
-    
-    public CardDealer(Cards[] deck, Player[] players, int round) {
-            this.players = players;
-            this.rounds = round;
+    private ArrayList<Integer> cards_chosen = new ArrayList<>();
+    private ArrayList<Integer> cardsOnTable = new ArrayList<>();
+
+    public CardDealer(Card[] deck, Player[] players, int round) {
+        this.players = players;
+        this.rounds = round;
+        this.deck = deck;
     }
-    
-    public Cards[] getDeckofCards(){
-        Cards[] deck = new Cards[52];
-        for (int j = 0; j < 4; j++) {
-            for (int i = 0; i < 12; i++) {
-                if (j == 0) {
-                    deck += Cards(i, Suits.SPADE);
-                } else if (j == 1) {
-                    deck += Cards(i, Suits.DIAMOND);
-                } else if (j == 2) {
-                    deck += Cards(i, Suits.HEART);
-                } else {
-                    deck += Cards(i, Suits.CLUB);
-                }
-            }
-        }
-        return deck;
-    }
-    
-    public Player[] getPlayers(){
+
+    public Player[] getPlayers() {
         return this.players;
     }
-    
-    public int getRounds(){
+
+    public int getRounds() {
         return this.rounds;
     }
-    
+
     public void completeRound() {
         this.rounds += 1;
     }
-    
-    public int getRandom(){
+
+    public int getRandom() {
         Random rand_int = new Random();
         int rand;
-        do{
+        do {
             rand = rand_int.nextInt(52);
         } while (cards_chosen.contains(rand));
-        cards_chosen.add(rand); 
+        cards_chosen.add(rand);
         return rand;
     }
-    
+
     public void giveCardstoPlayers() {
         Random rand_int = new Random();
         int rand_1;
         int rand_2;
-        for (int i =0; i < this.players.length; i++) {
+        for (int i = 0; i < this.players.length; i++) {
             rand_1 = rand_int.nextInt(52);
             rand_2 = rand_int.nextInt(52);
             this.players[i].setCard1(this.deck[this.getRandom()]);
             this.players[i].setCard2(this.deck[this.getRandom()]);
         }
     }
-    
-    public Cards placeCardsOnTable() {
-        int rand  = this.getRandom();
+
+    public Card placeCardsOnTable() {
+        int rand = this.getRandom();
         cardsOnTable.add(rand);
         return this.deck[rand];
     }
-    
-    
+
 }
-    
