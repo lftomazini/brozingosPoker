74f4090940efe45fafc68c14ce0865ddcd50e231
Anmol Singh <as062@bucknell.edu>
2015-11-19 21:22:26 -0500

    updated player class with "hand"

--- src/csci205finalproject/Player.java
+++ src/csci205finalproject/Player.java
@@ -28,149 +28,150 @@ public class Player {
     private boolean bigBlind;
     private boolean smallBlind;
     private boolean hasFolded;
+    private int money;
+    private Hand hand;
     Card card1;
     Card card2;
     private int[] chips = new int[Chips.values().length];
     ArrayList<Card> finalHand = new ArrayList<>();
+
+    public Player() {
+        this.bigBlind = false;
+        this.smallBlind = false;
+        this.card1 = null;
+        this.card2 = null;
+        this.hasFolded = false;
+    }
 
     public Player(int[] chips1) {
         this.bigBlind = false;
         this.smallBlind = false;
         this.card1 = null;
         this.card2 = null;
-        this.hasFolded = false;
-        this.chips = chips;
-    }
-
-    public Player(int[] chips1, Card card1, Card card2) {
-        this.bigBlind = false;
-        this.smallBlind = false;
-        this.card1 = card1;
-        this.card2 = card2;
         this.chips = chips;
         this.hasFolded = false;
+        this.hand = null;
+        this.money = this.getTotalChipsValue();
     }
-
-    public void removeBlackChip(int numHundred) {
-        if (this.chips[4] >= numHundred) {
-            this.chips[4] -= numHundred;
-        } else {
-            if (this.chips[3] > numHundred * (int) (Chips.BLACK.getValue() / Chips.GREEN.getValue())) {
-                this.chips[3] -= numHundred * (int) (Chips.BLACK.getValue() / Chips.GREEN.getValue());
-            } else {
-                if (this.chips[2] > numHundred * (int) (Chips.BLACK.getValue() / Chips.BLUE.getValue())) {
-                    this.chips[2] -= numHundred * (int) (Chips.BLACK.getValue() / Chips.BLUE.getValue());
-                } else {
-                    if (this.chips[1] > numHundred * (int) (Chips.BLACK.getValue() / Chips.RED.getValue())) {
-                        this.chips[1] -= numHundred * (int) (Chips.BLACK.getValue() / Chips.RED.getValue());
-                    } else {
-                        this.chips[0] -= numHundred * (int) (Chips.BLACK.getValue() / Chips.WHITE.getValue());
-                    }
-                }
-
-            }
-        }
-    }
-
-    public void removeGreenChip(int TwentyFive) {
-        if (this.chips[3] >= TwentyFive) {
-            this.chips[3] -= TwentyFive;
-        } else {
-            if (this.chips[2] > TwentyFive * (int) (Chips.GREEN.getValue() / Chips.BLUE.getValue())) {
-                this.chips[2] -= TwentyFive * (int) (Chips.GREEN.getValue() / Chips.BLUE.getValue());
-            } else {
-                if (this.chips[1] > TwentyFive * (int) (Chips.GREEN.getValue() / Chips.RED.getValue())) {
-                    this.chips[1] -= TwentyFive * (int) (Chips.GREEN.getValue() / Chips.RED.getValue());
-                } else {
-                    this.chips[0] -= TwentyFive * (int) (Chips.GREEN.getValue() / Chips.WHITE.getValue());
-                }
-            }
-
-        }
-    }
-
-    public void removeBlueChip(int Ten) {
-        if (this.chips[2] >= Ten) {
-            this.chips[2] -= Ten;
-        } else {
-            if (this.chips[1] > Ten * (int) (Chips.BLUE.getValue() / Chips.RED.getValue())) {
-                this.chips[1] -= Ten * (int) (Chips.BLUE.getValue() / Chips.RED.getValue());
-            } else {
-                this.chips[0] -= Ten * (int) (Chips.BLUE.getValue() / Chips.WHITE.getValue());
-            }
-        }
-    }
-
-    public void removeRedChip(int Five) {
-        if (this.chips[1] >= Five) {
-            this.chips[1] -= Five;
-        } else {
-            this.chips[0] -= Five * (int) (Chips.RED.getValue() / Chips.WHITE.getValue());
-        }
-    }
-
-    public void removeWhiteChip(int One) {
-        this.chips[0] -= One;
-    }
-
-    public int getTotalChipsValue() {
-        int value = 0;
-        value += this.chips[0] * Chips.WHITE.getValue();
-        value += this.chips[1] * Chips.RED.getValue();
-        value += this.chips[2] * Chips.BLUE.getValue();
-        value += this.chips[3] * Chips.GREEN.getValue();
-        value += this.chips[4] * Chips.BLACK.getValue();
-        return value;
-    }
-
-    public int makeBet(int bet) {
-        int betMade = bet;
-
-        if (bet > this.getTotalChipsValue()) {
-            return -1;
-        } else {
-            int numHundred = bet % (int) (Chips.BLACK.getValue());
-            bet = bet - (int) (Chips.BLACK.getValue() * numHundred);
-
-            int numTwentyFive = bet % (int) (Chips.GREEN.getValue());
-            bet = bet - (int) (Chips.GREEN.getValue() * numTwentyFive);
-
-            int numTen = bet % (int) (Chips.BLUE.getValue());
-            bet = bet - (int) (Chips.BLUE.getValue() * numTen);
-
-            int numFive = bet % (int) (Chips.RED.getValue());
-            bet = bet - (int) (Chips.RED.getValue() * numFive);
-
-            int numOne = bet % (int) (Chips.WHITE.getValue());
-            bet = bet - (int) (Chips.WHITE.getValue() * numOne);
-
-            if (numHundred != 0) {
-                this.removeBlackChip(numHundred);
-            }
-            if (numTwentyFive != 0) {
-                this.removeGreenChip(numTwentyFive);
-            }
-            if (numTen != 0) {
-                this.removeBlueChip(numTen);
-            }
-            if (numFive != 0) {
-                this.removeRedChip(numFive);
-            }
-            if (numOne != 0) {
-                this.removeWhiteChip(numOne);
-            }
-        }
-        return betMade;
-    }
-
-//    public void colorDown() {
-//        for (int i = 3; i < 1; i--) {
-//            if (this.chips[i] == 0 && this.chips[i+1] > 0) {
-//                this.chips[i] += this.chips[i+1]*(this.chips)
-//            }
+    
+//    public void removeBlackChip(int numHundred) {
+//        if (this.chips[4] >= numHundred) {
+//            this.chips[4] -= numHundred;
+//        } else {
+//            if (this.chips[3] > numHundred*(int)(Chips.BLACK.getValue()/Chips.GREEN.getValue())) {
+//                this.chips[3] -= numHundred*(int)(Chips.BLACK.getValue()/Chips.GREEN.getValue());
+//            } else {
+//                if (this.chips[2] > numHundred*(int)(Chips.BLACK.getValue()/Chips.BLUE.getValue())) {
+//                    this.chips[2] -= numHundred*(int)(Chips.BLACK.getValue()/Chips.BLUE.getValue());
+//                } else {
+//                    if (this.chips[1] > numHundred*(int)(Chips.BLACK.getValue()/Chips.RED.getValue())) {
+//                        this.chips[1] -= numHundred*(int)(Chips.BLACK.getValue()/Chips.RED.getValue());
+//                    } else {
+//                        this.chips[0] -= numHundred*(int)(Chips.BLACK.getValue()/Chips.WHITE.getValue());
+//                    } 
+//                }
 //
+//            } 
 //        }
 //    }
+//    
+//    public void removeGreenChip(int TwentyFive) {
+//        if (this.chips[3] >= TwentyFive) {
+//            this.chips[3] -= TwentyFive;
+//        } else {
+//            if (this.chips[2] > TwentyFive*(int)(Chips.GREEN.getValue()/Chips.BLUE.getValue())) {
+//                this.chips[2] -= TwentyFive*(int)(Chips.GREEN.getValue()/Chips.BLUE.getValue());
+//            } else {
+//                if (this.chips[1] > TwentyFive*(int)(Chips.GREEN.getValue()/Chips.RED.getValue())) {
+//                    this.chips[1] -= TwentyFive*(int)(Chips.GREEN.getValue()/Chips.RED.getValue());
+//                } else {
+//                        this.chips[0] -= TwentyFive*(int)(Chips.GREEN.getValue()/Chips.WHITE.getValue());
+//                    } 
+//                }
+//
+//        } 
+//    }
+//    
+//    public void removeBlueChip(int Ten) {
+//        if (this.chips[2] >= Ten) {
+//            this.chips[2] -= Ten;
+//        } else {
+//            if (this.chips[1] > Ten*(int)(Chips.BLUE.getValue()/Chips.RED.getValue())) {
+//                this.chips[1] -= Ten*(int)(Chips.BLUE.getValue()/Chips.RED.getValue());
+//            } else {
+//                    this.chips[0] -= Ten*(int)(Chips.BLUE.getValue()/Chips.WHITE.getValue());
+//                } 
+//            }
+//    }
+//    
+//    public void removeRedChip(int Five) {
+//        if (this.chips[1] >= Five) {
+//            this.chips[1] -= Five;
+//        } else {
+//            this.chips[0] -= Five*(int)(Chips.RED.getValue()/Chips.WHITE.getValue());
+//        } 
+//    }
+//    
+//    public void removeWhiteChip(int One) {
+//        this.chips[0] -= One;
+//    }
+    
+    public int getTotalChipsValue() {
+        int value = 0;
+        value += this.chips[0]*Chips.WHITE.getValue();
+        value += this.chips[1]*Chips.RED.getValue();
+        value += this.chips[2]*Chips.BLUE.getValue();
+        value += this.chips[3]*Chips.GREEN.getValue();
+        value += this.chips[4]*Chips.BLACK.getValue();
+        return value;
+    }
+    
+    public void getChipsFromMoney(){
+        int value = this.money;
+        
+        int numHundred = value%(int)(Chips.BLACK.getValue());
+        value = value - (int)(Chips.BLACK.getValue()*numHundred);
+        this.chips[4] = numHundred;
+        
+        int numTwentyFive = value%(int)(Chips.GREEN.getValue());
+        value = value - (int)(Chips.GREEN.getValue()*numTwentyFive);
+        this.chips[3] = numTwentyFive;
+        
+        int numTen = value%(int)(Chips.BLUE.getValue());
+        value = value - (int)(Chips.BLUE.getValue()*numTen);
+        this.chips[2] = numTen;
+        
+        int numFive = value%(int)(Chips.RED.getValue());
+        value = value - (int)(Chips.RED.getValue()*numFive);
+        this.chips[1] = numFive;
+        
+        int numOne = value%(int)(Chips.WHITE.getValue());
+        value = value - (int)(Chips.WHITE.getValue()*numOne);
+        this.chips[0] = numOne;
+    }
+    
+    public int makeBet(int bet) {
+        if (bet > this.money) {
+            return -1;
+        } else {
+            this.money -= bet;
+            this.getChipsFromMoney();    
+        }
+        return bet;
+    }
+    
+    public void check(){
+    }
+    
+    public void call(int bet){
+        int money = this.makeBet(bet);
+    }
+    
+    public void fold(){
+        this.setCard1(null);
+        this.setCard2(null);
+    }
+    
     public void setBigBlind(boolean bigBlind) {
         this.bigBlind = bigBlind;
     }
@@ -182,6 +183,10 @@ public class Player {
     public void setCard1(Card card1) {
         this.card1 = card1;
     }
+    
+    public void setHand(Hand hand){
+        this.hand = hand;
+    }
 
     public void setCard2(Card card2) {
         this.card2 = card2;
@@ -191,6 +196,10 @@ public class Player {
         this.chips = chips;
     }
 
+    public void setMoney(int money) {
+        this.money = money;
+    }
+    
     public boolean isBigBlind() {
         return bigBlind;
     }
@@ -209,5 +218,13 @@ public class Player {
 
     public int[] getChips() {
         return this.chips;
+    }
+    
+    public int getMoney() {
+        return this.money;
+    }
+    
+    public Hand getHand(){
+        return this.hand;
     }
 }
